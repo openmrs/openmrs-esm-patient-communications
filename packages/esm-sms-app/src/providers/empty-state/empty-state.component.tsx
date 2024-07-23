526119d085b507872f3e896cc02d5c1db262f7a8
jexsie <jessiessebuliba@gmail.com>
2024-07-23 10:48:09 +0300

    fix up

--- packages/esm-sms-app/src/providers/empty-state/empty-state.component.tsx
+++ packages/esm-sms-app/src/providers/empty-state/empty-state.component.tsx
@@ -13,7 +13,7 @@ export interface EmptyStateProps {
 }
 
 export const EmptyState: React.FC<EmptyStateProps> = (props) => {
-  const { t } = useTranslation('@openmrs/esm-patient-chart-app');
+  const { t } = useTranslation();
   const isTablet = useLayoutType() === 'tablet';
 
   return (
